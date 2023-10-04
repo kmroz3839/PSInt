617442c46abe6180057494e5ff74227533b2831c
counter185 <33550839+counter185@users.noreply.github.com>
2023-10-04 19:46:52 +0200

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import pandas as pd
+import json
+
 
 # -------------------------
 # zadanie 1
@@ -10,10 +12,13 @@ def check_url(url: str) -> bool:
     response = requests.get(url)
     return response.status_code >= 200 and response.status_code < 300
 
+
 # -------------------------
 # zadanie 2
 
-weather_url = "https://www.meteoprog.pl/pl/weather/Olsztyn/"
+miasto = "olsztyn"
+
+weather_url = f"https://www.meteoprog.pl/pl/weather/{miasto}/"
 w_res = requests.get(weather_url)
 
 w_s = BeautifulSoup(w_res.content.decode('utf-8'), features="html.parser")
@@ -33,4 +38,30 @@ plt.plot(pd.Series(data={ x: y for (x, y) in inne_temp}, index=[x for (x, y) in 
 plt.ylabel("temperatura")
 plt.show()
 
-#print(inne_temp)
+
+# -------------------------
+# zadanie 3
+
+stacja_id = 5766
+api_url = f'https://api.gios.gov.pl/pjp-api/rest/data/getData/{stacja_id}'
+
+response = requests.get(api_url)
+
+api_json = json.loads(response.content.decode("utf-8"))
+api_values = [(x["date"], float(x["value"])) for x in api_json["values"] if x["value"] is not None]
+
+api_valueseries = pd.Series(
+    data={x: y for (x,y) in api_values}, 
+    index=[x for (x,y) in api_values])
+
+plt.plot(api_valueseries)
+plt.ylabel("pomiar")
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
+
+# -------------------------
+# zadanie 4
+
+# na pewno jest to czytanie jsonów....
+# strony html częściej i bardziej się zmieniają
