37d4e668efc5b9e793a1aff817f5cfa0684b7ab3
counter185 <33550839+counter185@users.noreply.github.com>
2023-11-08 22:04:19 +0100

    games list page + stat

--- teamfinder/webapp_akari/urls.py
+++ teamfinder/webapp_akari/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("akari_logout", views.akari_logout, name="akari_logout"),
+    path("games", views.games_list, name="games_list"),
     path("game/<int:game_id>/", views.game_detail, name="game_detail"),
     path("game/<int:game_id>/submit", views.user_submit, name="user_submit")
 ]
--- teamfinder/webapp_akari/views.py
+++ teamfinder/webapp_akari/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
+from django.contrib import auth
+from django.http import HttpResponseRedirect, HttpResponse
+from django.db.models import Count
+
+from .models import GameEntry, UserSubmission
 
 # Create your views here.
 def index(request):
     #template = loader.get_template("akari/mainpage.html")
     #return HttpResponse(template.render({}, request))
     return render(request, "akari/mainpage.html", {})
+
+def akari_logout(request):
+    auth.logout(request)
+    #return HttpResponseRedirect("/")
+    return render(request, "registration/logout.html", {})
+
+class GameListEntry:
+    num: int
+    name: str
+    id: int
+    def __init__(self, num, name, id):
+        self.num = num
+        self.name = name
+        self.id = id
+
+def games_list(request):
+    gameStats = sorted([GameListEntry(num=UserSubmission.objects.filter(game_id=x.id).count(), name=x.name, id=x.id) for x in GameEntry.objects.all()], key= lambda x: x.num)[::-1]
+
+    return render(request, "akari/gamelist.html", {
+        "gamelist": gameStats
+    })
 
 def game_detail(request, game_id):
     return HttpResponse(f"[akari] Viewing details for {game_id}")
