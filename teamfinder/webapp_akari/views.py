a4efa6f0015f7121b49636ef5694c0b21a796090
counter185 <33550839+counter185@users.noreply.github.com>
2023-11-08 15:04:32 +0100

    css/pagerender template

--- teamfinder/webapp_akari/views.py
+++ teamfinder/webapp_akari/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("[akari] index page")
+    #template = loader.get_template("akari/mainpage.html")
+    #return HttpResponse(template.render({}, request))
+    return render(request, "akari/mainpage.html", {})
 
 def game_detail(request, game_id):
     return HttpResponse(f"[akari] Viewing details for {game_id}")
