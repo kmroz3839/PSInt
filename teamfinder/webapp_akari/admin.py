d6a9a1de2f2f9075bad2f6f4ffa896ee9a1266ae
counter185 <33550839+counter185@users.noreply.github.com>
2024-01-01 17:57:06 +0100

    remove old project

--- teamfinder/webapp_akari/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import GameEntry, UserSubmission
-
-# Register your models here.
-admin.site.register(GameEntry)
-admin.site.register(UserSubmission)
